5504eebb450d989a03569cb307b449da4ed1a67d
TechPrime Developers <suporte@techprimedev.tech>
2020-02-29 17:09:51 -0300

    Basic Build

---
+++ CSGO Cheat Loader OpenSource v1 by Techprimedev/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSGO_Cheat_Loader_OpenSource_v1_by_Techprimedev
+{
+    static class Program
+    {
+        /// <summary>
+        /// Ponto de entrada principal para o aplicativo.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
